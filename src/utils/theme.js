d6a90605e736cb091ebcf083d697009f7294e203
Iuliia Sokol <avare779@gmail.com>
2023-01-04 14:48:53 +0200

    theme replaced to separated file

---
+++ src/utils/theme.js
@@ -0,0 +1,69 @@
+export const theme = {
+  colors: {
+    mainDark: '#000000',
+    mainLight: '#FFFFFF',
+    mainAccent: '#F3C653',
+    secondaryAccent: '#C79002',
+
+    headersMain: '#494848',
+    supheader: '#B4B4B4',
+    textContent: '#7A7A7A',
+    textDataHero: '#E5E5E5',
+
+    footerBg: '#0E0C07',
+    sectionBg1: '#FFFDEE',
+    sectionBg2: '#F0ECD6',
+    btnDisabledBg: 'rgba(0, 0, 0, 0.4)',
+
+    headerBackdrops: 'rgba(0, 0, 0, 0.65)',
+    homePageSectionBackdrop: 'rgba(0, 0, 0, 0.76)',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  sizes: {
+    btnStandartWidth: '236px',
+    btnStandartHeight: '48px',
+    btnLargeHeight: '65px',
+    btnModalHeight: '64px',
+  },
+  fonts: {
+    logo: 'Film P3',
+    main: 'Montserrat, sans-serif',
+    secondary: 'Times New Roman',
+  },
+  fontSizes: {
+    xxl: '128px',
+    xl: '64px',
+    l: '40px',
+    m: '36px',
+    s: '32px',
+    xs: '20px',
+    xxs: '16px',
+    xxxs: '14px',
+  },
+  lineHeights: {
+    logo: '1.08',
+    subheader: '1.14',
+    supheader: '1.16',
+    content: '1.25',
+  },
+  letterSpacings: {
+    logo: '0.3em',
+    subheader: '0.05em',
+    content: '0.02em',
+    textDataHero: '0.01em',
+  },
+
+  borders: {
+    btnStandart: '2px solid #FFFFFF',
+    btnStandartActive: '2px solid #F3C653',
+    btnLarge: '1px solid #F3C653',
+    btnLargeActive: '1px solid #F3C653',
+  },
+  radii: {
+    btnStandart: '25px',
+    btnModal: '2px',
+  },
+  transitions: {
+    main: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
