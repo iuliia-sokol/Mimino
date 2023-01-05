64780a74c0ff94872bcd4918952c5bd5a7d1e810
Iuliia Sokol <avare779@gmail.com>
2023-01-05 18:36:24 +0200

    section 1 styles added

--- src/components/HomepageComponents/Section1/Section1.styled.jsx
+++ src/components/HomepageComponents/Section1/Section1.styled.jsx
@@ -3,16 +3,18 @@ import styled from 'styled-components';
 export const Section1Wrapper = styled.section`
   width: 100%;
   padding-top: 16px;
+  padding-bottom: 20px;
   background-color: ${p => p.theme.colors.sectionBg1};
-  /* padding-bottom: 102px; */
 
   @media screen and (min-width: 1024px) {
     padding-top: 120px;
+    padding-bottom: 32px;
     /* padding-top: 225px;
     padding-bottom: 65px; */
   }
 
   @media screen and (min-width: 1280px) {
+    padding-bottom: 50px;
   }
 
   /* @media (min-device-pixel-ratio: 2),
@@ -26,19 +28,51 @@ export const DataWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  max-width: 258px;
+
+  @media screen and (min-width: 1024px) {
+    max-width: fit-content;
+  }
 `;
 
 export const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
   & > img {
-    transform: translateY(-153px);
-    box-shadow: 5px 5px 70px 17px rgba(0, 0, 0, 0.25);
+    width: 244px;
+    height: 322px;
+    margin-top: 24px;
+    box-shadow: 2px 2px 60px 2px rgba(0, 0, 0, 0.25);
     border-radius: 2px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    & > img {
+      width: 400px;
+      height: 524px;
+      margin-top: 0;
+      box-shadow: 5px 5px 70px 17px rgba(0, 0, 0, 0.25);
+      transform: translateY(-153px);
+    }
+  }
+
+  @media screen and (min-width: 1280px) {
+    & > img {
+      width: 460px;
+      height: 574px;
+    }
   }
 `;
 
 export const BlockWrapper = styled.div`
   display: flex;
-  gap: 35px; ;
+  flex-direction: column;
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: row;
+    gap: 35px;
+  }
 `;
 
 export const SubHeader = styled.h3`
