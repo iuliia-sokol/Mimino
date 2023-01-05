561c6af978720b01dbd6d265bcad6ed79b0c2925
iuliia-sokol <110597796+iuliia-sokol@users.noreply.github.com>
2023-01-05 14:41:39 +0000

    Deploying to gh-pages from @ iuliia-sokol/Mimino@78a67f41ef9e8be0edfe820f2e519717342fc953 🚀

--- static/js/856.be173b09.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmimino=self.webpackChunkmimino||[]).push([[856],{856:function(t,n,a){a.d(n,{$0r:function(){return r}});var i=a(983);function r(t){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"}}]})(t)}}}]);
-//# sourceMappingURL=856.be173b09.chunk.js.map
