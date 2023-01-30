b75087a510406dd3fc8e87c1e29cee4d9715c223
Iuliia Sokol <avare779@gmail.com>
2023-01-30 16:43:44 +0200

    modal check

--- src/components/RoomsComponents/Triple/Section1/Section1.jsx
+++ src/components/RoomsComponents/Triple/Section1/Section1.jsx
@@ -1,7 +1,8 @@
 import { ReactSVG } from 'react-svg';
 import ArrowRight from '../../../../images/icons/arrow-right.svg';
 import { ButtonLarge } from 'components/Buttons/ButtonLarge/ButtonLarge';
-import { useState } from 'react';
+// import { useState } from 'react';
+// import { Modal } from 'components/Modal/Modal';
 import {
   Section1Wrapper,
   ButtonWrapper,
@@ -16,16 +17,14 @@ import {
   Container,
 } from './Section1.styled';
 
-import { Modal } from 'components/Modal/Modal';
-
 export const Section1 = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [statusModal, setStatusModal] = useState('');
+  // const [showModal, setShowModal] = useState(false);
+  // const [statusModal, setStatusModal] = useState('');
 
-  const toggleModal = status => {
-    setShowModal(!showModal);
-    setStatusModal(status);
-  };
+  // const toggleModal = status => {
+  //   setShowModal(!showModal);
+  //   setStatusModal(status);
+  // };
   return (
     <Section1Wrapper>
       <Container>
@@ -103,7 +102,7 @@ export const Section1 = () => {
         <ButtonWrapper>
           <ButtonLarge
             text="ОФОРМИТИ БРОНЮВАННЯ"
-            onClick={() => toggleModal('room')}
+            // onClick={() => toggleModal('room')}
           />
           <GoBackLink to="/hotel">
             <span>Повернутися до перегляду номерів</span>
@@ -111,7 +110,7 @@ export const Section1 = () => {
           </GoBackLink>
         </ButtonWrapper>
       </Container>
-      {showModal && <Modal closeModal={toggleModal} status={statusModal} />}
+      {/* {showModal && <Modal closeModal={toggleModal} status={statusModal} />} */}
     </Section1Wrapper>
   );
 };
